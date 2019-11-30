fb1e5900615565e832ce57b1e4e73554dbd8b342
BergNovis <jacktwist.ennis@outlook.com>
2019-11-30 15:38:58 +0800

    进行了一些测试

--- gwent.py
+++ gwent.py
@@ -22,24 +22,34 @@ def get_page_cards(page):
 
 #将结果写入cards.txt
 def write_to_file(card):
-    with open('cards.txt','a',encoding='utf8') as f:
+    with open('cards.py','a',encoding='utf8') as f:
         f.write(json.dumps(card, ensure_ascii = False) + '\n')
         f.close()
 
-
+# 从源码中提取卡牌信息
 def parse_page_cards(html):
     data = json.loads(html)  #把请求结果中的json字符串转为字典
     cards = data['data']['cards'] #取出字典结果中的'data'的value
-    for card in cards:
-        write_to_file(card)
+    return cards
 
 
 
+# 主函数
 def main():
-    for x in range(0, 28):
-        html = get_page_cards(x)
-        print(html)
-        parse_page_cards(html)
+    html = get_page_cards(0)
+    cards = parse_page_cards(html)
+    for card in cards:
+        if 'attribute' in card:
+            print('存在')
+        else:
+            print('不存在')
+        write_to_file(card)
+    # for x in range(0, 28):
+    #     html = get_page_cards(x)
+    #     # print(html)
+    #     cards = parse_page_cards(html)
+    #     for card in cards: #card类型是dic
+    #         write_to_file(card)
 
 if __name__ == '__main__':
     main()
