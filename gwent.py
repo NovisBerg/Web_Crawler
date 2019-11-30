1ebfccd774377da214cb23eb64b7118026f87ff1
BergNovis <jacktwist.ennis@outlook.com>
2019-11-30 18:13:12 +0800

    将写入本地改为写入数据库，添加了数据库配置文件conf4gwent.py并改动了gwent.py，已实现'爬取'这个功能，待加入多线程功能

--- gwent.py
+++ gwent.py
@@ -3,6 +3,8 @@ import json
 import requests
 from requests.exceptions import RequestException
 
+import conf4gwent as Conn
+
 def get_page_cards(page):
     form = {
         'token' : '',
@@ -21,10 +23,10 @@ def get_page_cards(page):
         return None
 
 #将结果写入cards.txt
-def write_to_file(card):
-    with open('cards.py','a',encoding='utf8') as f:
-        f.write(json.dumps(card, ensure_ascii = False) + '\n')
-        f.close()
+# def write_to_file(card):
+#     with open('cards.txt','a',encoding='utf8') as f:
+#         f.write(json.dumps(card, ensure_ascii = False) + '\n')
+#         f.close()
 
 # 从源码中提取卡牌信息
 def parse_page_cards(html):
@@ -36,20 +38,24 @@ def parse_page_cards(html):
 
 # 主函数
 def main():
-    html = get_page_cards(0)
-    cards = parse_page_cards(html)
-    for card in cards:
-        if 'attribute' in card:
-            print('存在')
-        else:
-            print('不存在')
-        write_to_file(card)
-    # for x in range(0, 28):
-    #     html = get_page_cards(x)
-    #     # print(html)
-    #     cards = parse_page_cards(html)
-    #     for card in cards: #card类型是dic
-    #         write_to_file(card)
+    card_num = 1
+    for x in range(0, 28):
+        html = get_page_cards(x)
+        # print(html)
+        cards = parse_page_cards(html)
+        for card in cards: #card类型是dic
+            cursor = Conn.conn.cursor()
+
+            sql = 'insert into cards values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+            cursor.execute(sql, (card_num, card.get('attribute'), card.get('clane'), card.get('cname'), card.get('deckable'), card.get('decompose'),
+                                 card.get('ename'), card.get('faction'), card.get('gwentlemenScore'), card.get('img'), card.get('kind'),
+                                 card.get('loyalty'), card.get('rarity'), card.get('recruit'), card.get('rule'), card.get('series'),
+                                 card.get('seriesAbbr'), card.get('seriesName'), card.get('seriesSize'), card.get('sysScore'), card.get('thumbnail')))
+            Conn.conn.commit()
+            card_num = card_num + 1
+            cursor.close()
+    Conn.conn.close()
+    print('卡牌数据已存入数据库')
 
 if __name__ == '__main__':
     main()
